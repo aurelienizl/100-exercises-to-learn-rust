ec305339a2abbe64163eea10820170330def717c
Aurélien Izoulet <aurelien.izoulet@epita.fr>
2025-02-03 16:43:30 +0100

    saving_work

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,18 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    let mut res = 2;
+    let mut i = n;
+    while (i > 2 )
+    {
+        res *= i;
+        i -= 1;
+    }
+    return res;
 }
 
 #[cfg(test)]
